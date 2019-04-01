5858fbf4bc3c4c7dbb402fa2048ef9bc6cfbac38
Michael Franklin <michael@illusional.net>
2019-04-02 08:38:01 +1100

    Move strelka to janis_bioinformatics

--- workflows/strelka_pipeline.py
+++ workflows/strelka_pipeline.py
@@ -1,57 +1,34 @@
-import janis as p
+from janis import Workflow, Input, Step, Output
 
 from janis_bioinformatics.data_types import BamBai, FastaWithDict
-from janis_bioinformatics.tools.bcftools import BcfToolsViewLatest
-from janis_bioinformatics.tools.common import SplitMultiAllele
-from janis_bioinformatics.tools.illumina import Strelka_2_9_9
-from janis_bioinformatics.tools.illumina.manta.manta_1_4_0 import Manta_1_4_0
+from janis_bioinformatics.tools.variantcallers import StrelkaVariantCaller
 
 
-class StrelkaSubworkflow(p.Workflow):
+class StrelkaWrapped(Workflow):
 
     @staticmethod
     def version():
         return "v1.0.0"
     
     def __init__(self):
-        p.Workflow.__init__(self, "strelka_pipeline")
+        Workflow.__init__(self, "strelka_pipeline")
     
-        ref = p.Input("reference", FastaWithDict())
-        bam = p.Input("bam", BamBai())
-    
-        s1_manta = p.Step("manta", Manta_1_4_0())
-        s2_strelka = p.Step("strelka", Strelka_2_9_9())
-        s3_bcf = p.Step("bcf", BcfToolsViewLatest())
-        s4_split = p.Step("splitmultiallele", SplitMultiAllele())
-    
-        # step 1
+        bam = Input("bam", BamBai(), "/Users/franklinmichael/Desktop/workflows-for-testing/wgs/inputs/intermediate/processed.bam")
+        ref = Input("reference", FastaWithDict(), "/Users/franklinmichael/reference/hg38/assembly_contigs_renamed/Homo_sapiens_assembly38.fasta")
+
+        strelka_vc = Step("strelka", StrelkaVariantCaller())
+
         self.add_edges([
-            (bam, s1_manta),
-            (ref, s1_manta)
+            (bam, strelka_vc),
+            (ref, strelka_vc)
         ])
-    
-        # step 2
+
         self.add_edges([
-            (bam, s2_strelka),
-            (ref, s2_strelka),
-            (s1_manta.candidateSmallIndels, s2_strelka.indelCandidates),
-            (s2_strelka.script, p.Output("script")),
-            (s2_strelka.stats, p.Output("stats")),
-            (s2_strelka.variants, p.Output("variants"))
-        ])
-    
-        # Step2
-        self.add_edge(s2_strelka.variants, s3_bcf.file)
-        self.add_default_value(s3_bcf.applyFilters, ["PASS"])
-    
-        # Step3
-        self.add_edges([
-            (ref, s4_split.reference),
-            (s3_bcf, s4_split.input),
-            (s4_split, p.Output("split-out"))
+            (strelka_vc.variants, Output("variants")),
+            (strelka_vc.out, Output("out"))
         ])
 
 
 if __name__ == "__main__":
-    w = StrelkaSubworkflow()
-    w.dump_translation("cwl", to_disk=True, with_docker=True)
+    w = StrelkaWrapped()
+    w.dump_translation("wdl", to_disk=True, write_inputs_file=True)
