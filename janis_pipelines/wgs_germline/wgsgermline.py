fbe083559d0cdfba86a2f1bea74bbe2adee467ec
Michael Franklin <michael.franklin@petermac.org>
2020-03-05 13:33:48 +1100

    Add documentation to wgs_germline pipeline

--- janis_pipelines/wgs_germline/wgsgermline.py
+++ janis_pipelines/wgs_germline/wgsgermline.py
@@ -41,26 +41,78 @@ class WGSGermlineMultiCallers(BioinformaticsWorkflow):
 
     def constructor(self):
 
-        self.input("fastqs", Array(FastqGzPair))
-        self.input("reference", FastaWithDict)
+        self.input(
+            "sample_name",
+            String,
+            doc="Sample name from which to generate the readGroupHeaderLine for BwaMem",
+        )
 
-        self.input("cutadapt_adapters", File)
+        self.input(
+            "fastqs",
+            Array(FastqGzPair),
+            doc="An array of FastqGz pairs. These are aligned separately and merged to create higher depth coverages from multiple sets of reads",
+        )
+        self.input(
+            "reference",
+            FastaWithDict,
+            doc="The reference genome from which to align the reads. This requires a number indexes (can be generated with the 'IndexFasta' pipeline. This pipeline has been tested with the hg38 reference genome.",
+        )
+        self.input(
+            "cutadapt_adapters",
+            File(optional=True),
+            doc="Specifies a file which contains a list of sequences to determine valid overrepresented sequences from the FastQC report to trim with Cuatadapt. The file must contain sets of named adapters in the form: ``name[tab]sequence``. Lines prefixed with a hash will be ignored.",
+        )
+        self.input(
+            "gatk_intervals",
+            Array(Bed),
+            doc="List of intervals over which to split the GATK variant calling",
+        )
+        self.input(
+            "vardict_intervals",
+            Array(Bed),
+            doc="List of intervals over which to split the VarDict variant calling",
+        )
+        self.input(
+            "strelka_intervals",
+            BedTabix,
+            doc="An interval for which to restrict the analysis to. Recommended HG38 interval: ",
+        )
 
-        self.input("gatk_intervals", Array(Bed))
-        self.input("vardict_intervals", Array(Bed))
-        self.input("strelkaIntervals", BedTabix)
+        self.input(
+            "header_lines",
+            File(optional=True),
+            doc="Header lines passed to BCFTools annotate as ``--header-lines``.",
+        )
 
-        self.input("header_lines", File)
-
-        self.input("sample_name", String, default="NA12878")
-        self.input("allele_freq_threshold", Float, default=0.05)
+        self.input(
+            "allele_freq_threshold",
+            Float,
+            default=0.05,
+            doc="The threshold for VarDict's allele frequency, default: 0.05 or 5%",
+        )
 
         # self.input("gridssBlacklist", Bed)
 
-        self.input("snps_dbsnp", VcfTabix)
-        self.input("snps_1000gp", VcfTabix)
-        self.input("known_indels", VcfTabix)
-        self.input("mills_indels", VcfTabix)
+        self.input(
+            "snps_dbsnp",
+            VcfTabix,
+            doc="From the GATK resource bundle, passed to BaseRecalibrator as ``known_sites``",
+        )
+        self.input(
+            "snps_1000gp",
+            VcfTabix,
+            doc="From the GATK resource bundle, passed to BaseRecalibrator as ``known_sites``",
+        )
+        self.input(
+            "known_indels",
+            VcfTabix,
+            doc="From the GATK resource bundle, passed to BaseRecalibrator as ``known_sites``",
+        )
+        self.input(
+            "mills_indels",
+            VcfTabix,
+            doc="From the GATK resource bundle, passed to BaseRecalibrator as ``known_sites``",
+        )
 
         # STEPS
 
@@ -116,7 +168,7 @@ class WGSGermlineMultiCallers(BioinformaticsWorkflow):
             IlluminaGermlineVariantCaller(
                 bam=self.merge_and_mark.out,
                 reference=self.reference,
-                intervals=self.strelkaIntervals,
+                intervals=self.strelka_intervals,
             ),
         )
 
@@ -162,34 +214,59 @@ class WGSGermlineMultiCallers(BioinformaticsWorkflow):
         )
         self.step("sort_combined", BcfToolsSort_1_9(vcf=self.combine_variants.vcf))
 
-        self.output("reports", source=self.fastqc.out, output_folder="reports")
-        self.output("bam", source=self.merge_and_mark.out, output_folder="bams")
-
         self.output(
-            "variants_combined", source=self.sort_combined.out, output_folder="variants"
+            "reports",
+            source=self.fastqc.out,
+            output_folder="reports",
+            doc="A zip file of the FastQC quality report.",
+        )
+        self.output(
+            "bam",
+            source=self.merge_and_mark.out,
+            output_folder="bams",
+            doc="Aligned and indexed bam.",
         )
 
         self.output(
-            "variants_gatk", source=self.vc_gatk_merge.out, output_folder="variants"
+            "variants_combined",
+            source=self.sort_combined.out,
+            output_folder="variants",
+            doc="Combined variants from all 3 callers",
+        )
+
+        self.output(
+            "variants_gatk",
+            source=self.vc_gatk_merge.out,
+            output_folder="variants",
+            output_name="gatk",
+            doc="Merged variants from the GATK caller",
         )
         self.output(
             "variants_vardict",
             source=self.vc_vardict_merge.out,
             output_folder=["variants"],
+            output_name="vardict",
+            doc="Merged variants from the VarDict caller",
         )
         self.output(
-            "variants_strelka", source=self.vc_strelka.out, output_folder="variants"
+            "variants_strelka",
+            source=self.vc_strelka.out,
+            output_folder="variants",
+            output_name="strelka",
+            doc="Variants from the Strelka variant caller",
         )
 
         self.output(
             "variants_gatk_split",
             source=self.vc_gatk.out,
             output_folder=["variants", "gatk"],
+            doc="Unmerged variants from the GATK caller (by interval)",
         )
         self.output(
             "variants_vardict_split",
             source=self.vc_vardict.out,
             output_folder=["variants", "variants"],
+            doc="Unmerged variants from the VarDict caller (by interval)",
         )
 
         # self.output("variants_gridss", source=self.vc_gridss.out)
@@ -206,11 +283,45 @@ class WGSGermlineMultiCallers(BioinformaticsWorkflow):
             "vardict",
             "strelka",
         ]
-        meta.contributors = ["Michael Franklin"]
-        meta.dateUpdated = date(2019, 10, 16)
+        meta.contributors = ["Michael Franklin", "Richard Lupat", "Jiaan Yu"]
+        meta.dateCreated = date(2018, 12, 24)
+        meta.dateUpdated = date(2020, 3, 5)
+
         meta.short_documentation = (
             "A variant-calling WGS pipeline using GATK, VarDict and Strelka2."
         )
+        meta.documentation = """\
+This is a genomics pipeline to align sequencing data (Fastq pairs) into BAMs and call variants using:
+
+This workflow is a reference pipeline using the Janis Python framework (pipelines assistant).
+
+- Takes raw sequence data in the FASTQ format;
+- align to the reference genome using BWA MEM;
+- Marks duplicates using Picard;
+- Call the appropriate variant callers (GATK / Strelka / VarDict);
+- Outputs the final variants in the VCF format.
+
+**Resources**
+
+This pipeline has been tested using the HG38 reference set, available on Google Cloud Storage through:
+
+- https://console.cloud.google.com/storage/browser/genomics-public-data/references/hg38/v0/
+
+This pipeline expects the assembly references to be as they appear in that storage \
+    (".fai", ".amb", ".ann", ".bwt", ".pac", ".sa", "^.dict").
+The known sites (snps_dbsnp, snps_1000gp, known_indels, mills_indels) should be gzipped and tabix indexed.
+"""
+        meta.sample_input_overrides = {
+            "fastqs": [
+                ["sample1_R1.fastq.gz", "sample1_R2.fastq.gz"],
+                ["sample1_R1-TOPUP.fastq.gz", "sample1_R2-TOPUP.fastq.gz"],
+            ],
+            "reference": "Homo_sapiens_assembly38.fasta",
+            "snps_dbsnp": "Homo_sapiens_assembly38.dbsnp138.vcf.gz",
+            "snps_1000gp": "1000G_phase1.snps.high_confidence.hg38.vcf.gz",
+            "known_indels": "Homo_sapiens_assembly38.known_indels.vcf.gz",
+            "mills_indels": "Mills_and_1000G_gold_standard.indels.hg38.vcf.gz",
+        }
 
 
 if __name__ == "__main__":
