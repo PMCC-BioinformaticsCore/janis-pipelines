a4b0a903de082a571f03cab2e057ae9f660c5caa
Michael Franklin <michael.franklin@petermac.org>
2020-10-30 12:46:43 +1100

    Add skip_localising_secondary_files to fasta reference

--- janis_pipelines/reference/__init__.py
+++ janis_pipelines/reference/__init__.py
@@ -11,15 +11,16 @@ REFERENCE_INPUTS = {
         - HG38: https://console.cloud.google.com/storage/browser/genomics-public-data/references/hg38/v0/
 
     - (".fai", ".amb", ".ann", ".bwt", ".pac", ".sa", "^.dict").""",
-        "quality": InputQualityType.static,
+        "quality": "static",
         "example": "Homo_sapiens_assembly38.fasta",
         "source": {
             "hg38": "gs://genomics-public-data/references/hg38/v0/Homo_sapiens_assembly38.fasta"
         },
+        "skip_localising_secondary_files": True,
     },
     "snps_dbsnp": {
         "doc": "From the GATK resource bundle, passed to BaseRecalibrator as ``known_sites``",
-        "quality": InputQualityType.static,
+        "quality": "static",
         "example": "Homo_sapiens_assembly38.dbsnp138.vcf.gz",
         "source": {
             "hg38": "gs://genomics-public-data/references/hg38/v0/Homo_sapiens_assembly38.dbsnp138.vcf"
@@ -27,7 +28,7 @@ REFERENCE_INPUTS = {
     },
     "snps_1000gp": {
         "doc": "From the GATK resource bundle, passed to BaseRecalibrator as ``known_sites``. Accessible from the HG38 genomics-public-data google cloud bucket: https://console.cloud.google.com/storage/browser/genomics-public-data/references/hg38/v0/ ",
-        "quality": InputQualityType.static,
+        "quality": "static",
         "example": "1000G_phase1.snps.high_confidence.hg38.vcf.gz",
         "source": {
             "hg38": "gs://genomics-public-data/references/hg38/v0/1000G_phase1.snps.high_confidence.hg38.vcf.gz"
@@ -35,7 +36,7 @@ REFERENCE_INPUTS = {
     },
     "known_indels": {
         "doc": "From the GATK resource bundle, passed to BaseRecalibrator as ``known_sites``",
-        "quality": InputQualityType.static,
+        "quality": "static",
         "example": "Homo_sapiens_assembly38.known_indels.vcf.gz",
         "source": {
             "hg38": "gs://genomics-public-data/references/hg38/v0/Homo_sapiens_assembly38.known_indels.vcf.gz"
@@ -43,7 +44,7 @@ REFERENCE_INPUTS = {
     },
     "mills_indels": {
         "doc": "From the GATK resource bundle, passed to BaseRecalibrator as ``known_sites``",
-        "quality": InputQualityType.static,
+        "quality": "static",
         "example": "Mills_and_1000G_gold_standard.indels.hg38.vcf.gz",
         "source": {
             "hg38": "gs://genomics-public-data/references/hg38/v0/Mills_and_1000G_gold_standard.indels.hg38.vcf.gz"
@@ -51,7 +52,7 @@ REFERENCE_INPUTS = {
     },
     "gnomad": {
         "doc": "The genome Aggregation Database (gnomAD). This VCF must be compressed and tabix indexed. This is specific for your genome (eg: hg38 / br37) and can usually be found with your reference. For example for HG38, the Broad institute provide the following af-only-gnomad compressed and tabix indexed VCF: https://console.cloud.google.com/storage/browser/gatk-best-practices/somatic-hg38;tab=objects?prefix=af-only",
-        "quality": InputQualityType.static,
+        "quality": "static",
         "example": "af-only-gnomad.hg38.vcf.gz",
         "source": {
             "hg38": "gs://gatk-best-practices/somatic-hg38/af-only-gnomad.hg38.vcf.gz",
@@ -60,7 +61,7 @@ REFERENCE_INPUTS = {
     },
     "panel_of_normals": {
         "doc": "VCF file of sites observed in normal.",
-        "quality": InputQualityType.static,
+        "quality": "static",
         "example": "Mutect2-WGS-panel-b37.vcf",
         "source": {
             "b37": "gs://gatk-best-practices/somatic-b37/Mutect2-WGS-panel-b37.vcf",
@@ -73,22 +74,22 @@ REFERENCE_INPUTS = {
 INTERVAL_INPUTS = {
     "gatk_intervals": {
         "doc": "List of intervals over which to split the GATK variant calling",
-        "quality": InputQualityType.static,
+        "quality": "static",
         "example": "BRCA1.bed",
     },
     "vardict_intervals": {
         "doc": "List of intervals over which to split the VarDict variant calling",
-        "quality": InputQualityType.static,
+        "quality": "static",
         "example": "BRCA1.bed",
     },
     "strelka_intervals": {
         "doc": "An interval for which to restrict the analysis to.",
-        "quality": InputQualityType.static,
+        "quality": "static",
         "example": "BRCA1.bed.gz",
     },
     "gridss_blacklist": {
         "doc": "BED file containing regions to ignore. For more information, visit: https://github.com/PapenfussLab/gridss#blacklist",
-        "quality": InputQualityType.static,
+        "quality": "static",
         "source": {
             "hg19": "https://www.encodeproject.org/files/ENCFF001TDO/@@download/ENCFF001TDO.bed.gz",
             "GRCh38": "https://www.encodeproject.org/files/ENCFF356LFX/@@download/ENCFF356LFX.bed.gz",
@@ -105,7 +106,7 @@ WGS_INPUTS = {
                 Specifies a containment list for cutadapt, which contains a list of sequences to determine valid
                 overrepresented sequences from the FastQC report to trim with Cuatadapt. The file must contain sets
                 of named adapters in the form: ``name[tab]sequence``. Lines prefixed with a hash will be ignored.""",
-        "quality": InputQualityType.static,
+        "quality": "static",
         "example": "contaminant_list.txt",
         "source": "https://raw.githubusercontent.com/csf-ngs/fastqc/master/Contaminants/contaminant_list.txt",
     },
