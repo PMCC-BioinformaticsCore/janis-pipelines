4b3296d08c645dfa228cd5343d2fe30d5be3751b
Jiaan Yu <jiaan7.yu@gmail.com>
2022-02-11 16:03:52 +1100

    Add unit test

--- janis_pipelines/wgs_germline_gatk/wgsgermlinegatk.py
+++ janis_pipelines/wgs_germline_gatk/wgsgermlinegatk.py
@@ -8,7 +8,7 @@ from janis_bioinformatics.tools import BioinformaticsTool
 from janis_bioinformatics.tools.babrahambioinformatics import FastQC_0_11_8
 from janis_bioinformatics.tools.common import BwaAligner, MergeAndMarkBams_4_1_3
 from janis_bioinformatics.tools.pmac import ParseFastqcAdapters
-from janis_core import String, Array, File
+from janis_core import String, Array, File, WorkflowMetadata
 from janis_core.tool.test_classes import (
     TTestCase,
     TTestExpectedOutput,
@@ -122,42 +122,47 @@ class WGSGermlineGATK(WGSGermlineGATKVariantsOnly):
 
     def tests(self) -> Optional[List[TTestCase]]:
         parent_dir = "https://swift.rc.nectar.org.au/v1/AUTH_4df6e734a509497692be237549bbe9af/janis-test-data/bioinformatics"
-        germline_data = f"{parent_dir}/wgsgermline_data"
+        brca1_test_data = f"{parent_dir}/brca1_test/test_data"
 
         return [
             TTestCase(
                 name="brca1",
                 input={
-                    "sample_name": "NA12878",
-                    "reference": f"{germline_data}/Homo_sapiens_assembly38.chr17.fasta",
+                    "sample_name": "NA12878-BRCA1",
                     "fastqs": [
                         [
-                            f"{germline_data}/NA12878-BRCA1_R1.fastq.gz",
-                            f"{germline_data}/NA12878-BRCA1_R2.fastq.gz",
+                            f"{brca1_test_data}/NA12878-BRCA1_R1.fastq.gz",
+                            f"{brca1_test_data}/NA12878-BRCA1_R1.fastq.gz",
                         ]
                     ],
-                    "gatk_intervals": [f"{germline_data}/BRCA1.hg38.bed"],
-                    "known_indels": f"{germline_data}/Homo_sapiens_assembly38.known_indels.BRCA1.vcf.gz",
-                    "mills_indels": f"{germline_data}/Mills_and_1000G_gold_standard.indels.hg38.BRCA1.vcf.gz",
-                    "snps_1000gp": f"{germline_data}/1000G_phase1.snps.high_confidence.hg38.BRCA1.vcf.gz",
-                    "snps_dbsnp": f"{germline_data}/Homo_sapiens_assembly38.dbsnp138.BRCA1.vcf.gz",
-                    # Need to add adapter sequence file
-                    "contamination_file": f"{germline_data}/contaminant_list.txt",
+                    "reference": f"{brca1_test_data}/Homo_sapiens_assembly38.chr17.fasta",
+                    "gridss_blacklist": f"{brca1_test_data}/consensusBlacklist.hg38.chr17.bed",
+                    "gnomad": f"{brca1_test_data}/af-only-gnomad.hg38.BRCA1.vcf.gz",
+                    "gatk_intervals": [f"{brca1_test_data}/BRCA1.hg38.bed"],
+                    "known_indels": f"{brca1_test_data}/Homo_sapiens_assembly38.known_indels.BRCA1.vcf.gz",
+                    "mills_indels": f"{brca1_test_data}/Mills_and_1000G_gold_standard.indels.hg38.BRCA1.vcf.gz",
+                    "snps_1000gp": f"{brca1_test_data}/1000G_phase1.snps.high_confidence.hg38.BRCA1.vcf.gz",
+                    "snps_dbsnp": f"{brca1_test_data}/Homo_sapiens_assembly38.dbsnp138.BRCA1.vcf.gz",
+                    "contamination_file": f"{brca1_test_data}/contaminant_list.txt",
+                    "adapter_file": f"{brca1_test_data}/adapter_list.txt",
                 },
-                output=Vcf.basic_test("out_variants_bamstats", 51300, 230)
-                + Vcf.basic_test("out_variants_gatk_split", 50700, 221)
+                output=Array.array_wrapper(
+                    [ZipFile.basic_test("out_fastqc_R1_reports", 408000)]
+                )
+                + Array.array_wrapper(
+                    [ZipFile.basic_test("out_fastqc_R2_reports", 408000)]
+                )
                 + BamBai.basic_test("out_bam", 2822000, 49600)
                 + TextFile.basic_test(
                     "out_performance_summary",
                     948,
                     md5="575354942cfb8d0367725f9020181443",
                 )
+                + CompressedVcf.basic_test("out_variants_gatk", 11000, 223)
                 + Array.array_wrapper(
-                    [
-                        ZipFile.basic_test("out_fastqc_reports", 408000),
-                        ZipFile.basic_test("out_fastqc_reports", 408000),
-                    ]
-                ),
+                    [Vcf.basic_test("out_variants_gatk_split", 51000, 221)]
+                )
+                + Vcf.basic_test("out_variants_bamstats", 6900, 232),
             )
         ]
 
