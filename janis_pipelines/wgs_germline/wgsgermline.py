783db9c103732cc0da34d5c301f886bc648971ac
Jiaan Yu <jiaan7.yu@gmail.com>
2022-02-11 17:11:30 +1100

    Add unit test

--- janis_pipelines/wgs_germline/wgsgermline.py
+++ janis_pipelines/wgs_germline/wgsgermline.py
@@ -9,18 +9,15 @@ from janis_core.tool.test_classes import (
     TTestPreprocessor,
 )
 
-from janis_bioinformatics.data_types import FastqGzPair
+from janis_unix.data_types import TextFile, ZipFile
+
+from janis_bioinformatics.data_types import FastqGzPair, Bam, BamBai, Vcf, CompressedVcf
 
 from janis_pipelines.wgs_germline.wgsgermline_variantsonly import (
     WGSGermlineMultiCallersVariantsOnly,
     INPUT_DOCS,
 )
 from janis_pipelines.wgs_germline_gatk.wgsgermlinegatk import WGSGermlineGATK
-
-expected_performance_summary = """\
-Total reads,Mapped reads,% Reads mapped,% Mapped reads duplicates,Total reads minus duplicates,Mapped reads minus duplicates,Paired in sequencing,Read1,Read2,Properly paired,With itself and mate mapped,Singletons,With mate mapped to a different chr,With mate mapped to a different chr (mapQ>=5),% Reads OnTarget,% OnTarget duplicates,OnTarget paired in sequencing,OnTarget read1,OnTarget read2,OnTarget properly paired,OnTarget With itself and mate mapped,OnTarget singletons,OnTarget with mate mapped to a different chr,OnTarget with mate mapped to a different chr (mapQ>=5),% Target bases >=1-fold Coverage,% Target bases >=10-fold Coverage,% Target bases >=20-fold Coverage,% Target bases >=100-fold Coverage,Mean coverage for target bases,% Target bases with one fifth of mean coverage,Median Fragment Length
-19615,19582,99.83,0.12,19592,19559,19299,9645,9654,18386,19242,24,778,636,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,0.00,0.00,0.00,0.00,0.00,0.00,556
-"""
 
 expected_gridss_flagstat = """\
 7 + 0 in total (QC-passed reads + QC-failed reads)
@@ -110,69 +107,80 @@ The known sites (snps_dbsnp, snps_1000gp, known_indels, mills_indels) should be 
 """
 
     def tests(self) -> Optional[List[TTestCase]]:
-        bioinf_base = "https://swift.rc.nectar.org.au/v1/AUTH_4df6e734a509497692be237549bbe9af/janis-test-data/bioinformatics"
-        chr17 = f"{bioinf_base}/petermac_testdata"
-        hg38 = f"{bioinf_base}/hg38"
+        parent_dir = "https://swift.rc.nectar.org.au/v1/AUTH_4df6e734a509497692be237549bbe9af/janis-test-data/bioinformatics"
+        brca1_test_data = f"{parent_dir}/brca1_test/test_data"
 
         return [
             TTestCase(
                 name="brca1",
                 input={
-                    "sample_name": "NA12878",
-                    "reference": f"{chr17}/Homo_sapiens_assembly38.chr17.fasta",
+                    "sample_name": "NA12878-BRCA1",
                     "fastqs": [
                         [
-                            f"{chr17}/NA12878-BRCA1_R1.fastq.gz",
-                            f"{chr17}/NA12878-BRCA1_R2.fastq.gz",
+                            f"{brca1_test_data}/NA12878-BRCA1_R1.fastq.gz",
+                            f"{brca1_test_data}/NA12878-BRCA1_R1.fastq.gz",
                         ]
                     ],
-                    "gatk_intervals": [f"{chr17}/BRCA1.hg38.bed"],
-                    "vardict_intervals": [f"{hg38}/NA12878/BRCA1.intersect.bed"],
-                    "strelka_intervals": f"{chr17}/BRCA1.hg38.bed.gz",
-                    "known_indels": f"{chr17}/Homo_sapiens_assembly38.known_indels.BRCA1.vcf.gz",
-                    "mills_indels": f"{chr17}/Mills_and_1000G_gold_standard.indels.hg38.BRCA1.vcf.gz",
-                    "snps_1000gp": f"{chr17}/1000G_phase1.snps.high_confidence.hg38.BRCA1.vcf.gz",
-                    "snps_dbsnp": f"{chr17}/Homo_sapiens_assembly38.dbsnp138.BRCA1.vcf.gz",
-                    "cutadapt_adapters": f"{chr17}/contaminant_list.txt",
-                    "gridss_blacklist": f"{chr17}/consensusBlacklist.hg38.chr17.bed",
-                    "MINIMUM_PCT": f"",
+                    "reference": f"{brca1_test_data}/Homo_sapiens_assembly38.chr17.fasta",
+                    "gridss_blacklist": f"{brca1_test_data}/consensusBlacklist.hg38.chr17.bed",
+                    "gnomad": f"{brca1_test_data}/af-only-gnomad.hg38.BRCA1.vcf.gz",
+                    "gatk_intervals": [f"{brca1_test_data}/BRCA1.hg38.bed"],
+                    "known_indels": f"{brca1_test_data}/Homo_sapiens_assembly38.known_indels.BRCA1.vcf.gz",
+                    "mills_indels": f"{brca1_test_data}/Mills_and_1000G_gold_standard.indels.hg38.BRCA1.vcf.gz",
+                    "snps_1000gp": f"{brca1_test_data}/1000G_phase1.snps.high_confidence.hg38.BRCA1.vcf.gz",
+                    "snps_dbsnp": f"{brca1_test_data}/Homo_sapiens_assembly38.dbsnp138.BRCA1.vcf.gz",
+                    "contamination_file": f"{brca1_test_data}/contaminant_list.txt",
+                    "adapter_file": f"{brca1_test_data}/adapter_list.txt",
                 },
-                output=[
-                    TTestExpectedOutput(
-                        tag="out_variants",
-                        preprocessor=TTestPreprocessor.LinesDiff,
-                        file_diff_source=f"{chr17}/NA12878/brca1.germline.combined.vcf",
-                        operator=lambda actual, expected: all(
-                            a <= b for a, b in zip(actual, expected)
-                        ),
-                        expected_value=(1, 1),
-                    ),
-                    TTestExpectedOutput(
-                        "out_variants_gridss",
-                        preprocessor=TTestPreprocessor.FileContent,
-                        operator=operator.eq,
-                        expected_file=f"{chr17}/NA12878/brca1.germline.gridss.vcf",
-                    ),
-                    TTestExpectedOutput(
-                        tag="out_bam",
-                        preprocessor=TTestPreprocessor.Value,
-                        operator=Bam.equal,
-                        expected_value=f"{chr17}/NA12878/NA12878.bam",
-                    ),
-                    TTestExpectedOutput(
-                        tag="out_gridss_assembly",
-                        preprocessor=Bam.flagstat,
-                        operator=operator.eq,
-                        expected_value=expected_gridss_flagstat,
-                    ),
-                    TTestExpectedOutput(
-                        tag="out_performance_summary",
-                        preprocessor=TTestPreprocessor.FileContent,
-                        operator=operator.eq,
-                        expected_value=expected_performance_summary,
-                    ),
-                ],
+                output=Array.array_wrapper(
+                    [ZipFile.basic_test("out_fastqc_R1_reports", 408000)]
+                )
+                + Array.array_wrapper(
+                    [ZipFile.basic_test("out_fastqc_R2_reports", 408000)]
+                )
+                + BamBai.basic_test("out_bam", 2822000, 49600)
+                + TextFile.basic_test(
+                    "out_performance_summary",
+                    948,
+                    md5="575354942cfb8d0367725f9020181443",
+                )
+                + Bam.basic_test("out_gridss_assembly", 16000)
+                + Vcf.basic_test("out_variants_gridss", 32000, 130)
+                + CompressedVcf.basic_test("out_variants_gatk", 11000, 223)
+                + Array.array_wrapper(
+                    [Vcf.basic_test("out_variants_gatk_split", 51000, 221)]
+                )
+                # + CompressedVcf.basic_test("out_variants_strelka", 30995, 223)
+                + CompressedVcf.basic_test("out_variants_vardict", 19000, 260)
+                + Array.array_wrapper(
+                    [Vcf.basic_test("out_variants_vardict_split", 84000, 258)]
+                )
+                + Vcf.basic_test("out_variants_combined_bamstats", 99000, 307),
             )
+            #     output=[
+            #         TTestExpectedOutput(
+            #             tag="out_variants_combined_bamstats",
+            #             preprocessor=TTestPreprocessor.LinesDiff,
+            #             file_diff_source=f"{chr17}/NA12878/brca1.germline.combined.vcf",
+            #             operator=lambda actual, expected: all(
+            #                 a <= b for a, b in zip(actual, expected)
+            #             ),
+            #             expected_value=(1, 1),
+            #         ),
+            #         TTestExpectedOutput(
+            #             "out_variants_gridss",
+            #             preprocessor=TTestPreprocessor.FileContent,
+            #             operator=operator.eq,
+            #             expected_file=f"{chr17}/NA12878/brca1.germline.gridss.vcf",
+            #         ),
+            #         TTestExpectedOutput(
+            #             tag="out_bam",
+            #             preprocessor=TTestPreprocessor.Value,
+            #             operator=Bam.equal,
+            #             expected_value=f"{chr17}/NA12878/NA12878.bam",
+            #         ),
+            #     ],
+            # )
         ]
 
 
